c1dae2e482ee96d5be6f6b0a329c375bf1fa6c43
yuanbo <yuanbo_php@163.com>
2018-11-05 10:22:33 +0800

    test

--- app/Helpers/StateCode.php
+++ app/Helpers/StateCode.php
@@ -13,5 +13,8 @@ class StateCode {
     /******* 订单状态 *******/
     const ORDER_INIT                = 0;
 
-
+    /****** sms 验证码状态 ****/
+    const SMS_STATUS_INIT = 0; //验证码未使用
+    const SMS_STATUS_USE = 1;  //已使用
+    const SMS_STATUS_FAIL = 2; //发送失败
 }
